be6021c3b7baac1f6104c709756ca4181c3404f2
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-11-12 12:29:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* Primeira forma de fazer
+
 function tocaSomPom(){
     document.querySelector("#som_tecla_pom").play()
 }
@@ -8,4 +10,57 @@ function tocaSomClap(){
     document.querySelector("#som_tecla_clap").play()
 }
 
-document.querySelector("tecla_clap").onclick = tocaSomClap
+document.querySelector(".tecla_clap").onclick = tocaSomClap;
+
+function tocaSomTim(){
+    document.querySelector("#som_tecla_tim").play()
+}
+
+document.querySelector(".tecla_tim").onclick = tocaSomTim;
+
+function tocaSomPuff(){
+    document.querySelector("#som_tecla_puff").play()
+}
+
+document.querySelector(".tecla_puff").onclick = tocaSomPuff;
+
+function tocaSomSplash(){
+    document.querySelector("#som_tecla_splash").play()
+}
+
+document.querySelector(".tecla_splash").onclick = tocaSomSplash;
+
+function tocaSomToim(){
+    document.querySelector("#som_tecla_toim").play()
+}
+
+document.querySelector(".tecla_toim").onclick = tocaSomToim;
+
+function tocaSomPsh(){
+    document.querySelector("#som_tecla_psh").play()
+}
+
+document.querySelector(".tecla_psh").onclick = tocaSomPsh;
+
+function tocaSomTic(){
+    document.querySelector("#som_tecla_tic").play()
+}
+
+document.querySelector(".tecla_tic").onclick = tocaSomTic;
+
+function tocaSomTom(){
+    document.querySelector("#som_tecla_tom").play()
+}
+
+document.querySelector(".tecla_tom").onclick = tocaSomTom; */
+
+//Segunda forma de fazer - refatorado
+
+const keyList = document.querySelectorAll(".tecla")
+
+console.log(keyList[1])
+
+switch(keyList){
+    case keyList[0]:
+    
+}
