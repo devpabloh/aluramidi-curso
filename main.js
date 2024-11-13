5e288915e248d08d82fd1b12a6358064b123fe3d
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-11-13 11:54:51 -0300

    add funcionalidades

--- main.js
+++ main.js
@@ -94,5 +94,16 @@ for (let contador = 0; contador < keyList.length; contador++) {
     tecla.onclick = function (){
         tocaSom(idAudio)
     }
-    
+
+    tecla.onkeydown = function(evento){
+
+        if(evento.code == 'Space' || evento.code == "Enter"){
+            tecla.classList.add("ativa")
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove("ativa")
+    }
+
 }
